0ac9b97dfb5e5a60278b535d3c38680f252697e8
Charles Ellison <crelliso@gmail.com>
2020-11-22 16:15:34 -0600

    Remove graphql modules and node_packages

--- server/modules/genres/input.ts
+++
@@ -1,6 +0,0 @@
-import { Field, InputType } from "type-graphql";
-
-@InputType()
-export class GenresInput {
-  @Field(type => String, { nullable: true }) language: string;
-}
--- server/modules/pagination/types.ts
+++
@@ -1,8 +0,0 @@
-import { Field, Int, ObjectType } from "type-graphql";
-
-@ObjectType()
-export class Pagination {
-  @Field(type => Int, { nullable: true }) page: number;
-  @Field(type => Int, { nullable: true }) total_pages: number;
-  @Field(type => Int, { nullable: true }) total_results: number;
-}
