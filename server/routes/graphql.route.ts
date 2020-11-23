d050999a866325ed10b017a4d1f490223f3c53e6
Charles Ellison <crelliso@gmail.com>
2020-11-22 19:33:47 -0600

    Remove graphql routes

--- server/routes/graphql.route.ts
+++
@@ -1,8 +0,0 @@
-import { graphqlController, playgroundController } from '../controllers';
-
-import express from 'express';
-
-export const router = express.Router();
-
-router.all("/graphql", graphqlController);
-router.all("/playground", playgroundController);
