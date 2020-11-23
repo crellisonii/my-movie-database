4381555ce7085687f10a68c4be6951781a9a1963
Charles Ellison <crelliso@gmail.com>
2020-11-23 01:55:07 -0600

    update genres interfaces

--- server/interfaces/genres.interface.ts
+++ server/interfaces/genres.interface.ts
@@ -1,3 +1,14 @@
+// Input interfaces
+export interface GenresInput {
+  language?: string;
+}
+
+export interface GenresQueryString {
+  api_key: string;
+  language: string;
+}
+
+// Types interfaces
 interface Genre {
   id: number;
   name: string;
