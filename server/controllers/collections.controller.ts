2e1a1e08b58c1b5fb69e3ebee910e791e53d96b5
Charles Ellison <crelliso@gmail.com>
2020-11-22 21:51:24 -0600

    Create Collections Controllers

---
+++ server/controllers/collections.controller.ts
@@ -0,0 +1,48 @@
+import { CollectionInputQuery, CollectionQuery } from '../interfaces';
+import { NextFunction, Request, Response } from 'express';
+import axios, { AxiosRequestConfig } from 'axios';
+
+import { apiKey } from '../env';
+import { baseUrl } from '../constants/url.constant';
+
+const collectionUrl = 'collection'
+
+export const getCollectionsDetails = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.collectionId;
+    const { language }: CollectionInputQuery = req.query;
+    const url = `${baseUrl}${collectionUrl}/${id}`;
+    const params: CollectionQuery = {
+      api_key: apiKey,
+      language: language ? language : 'en-US'
+    };
+    const options: AxiosRequestConfig = {
+      method: 'GET',
+      params,
+      url
+    };
+    const resp = await axios(options);
+    res.json(resp.data);
+  }
+  catch (e) {
+    // console.log('getCollection error: \n', e);
+    throw new Error(e);
+  }
+}
+
+export const getCollectionsImages = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.collectionId;
+  const { language }: CollectionInputQuery = req.query;
+  const url = `${baseUrl}${collectionUrl}/${id}/images`;
+  const params: CollectionQuery = {
+    api_key: apiKey,
+    language: language ? language : 'en-US'
+  }
+  const options: AxiosRequestConfig = {
+    method: 'GET',
+    params,
+    url
+  };
+  const resp = await axios(options);
+  res.json(resp.data);
+}
