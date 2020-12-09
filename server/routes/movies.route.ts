7932ce3acdc212e284480e44d0f96aaa064e90d0
Charles Ellison <crelliso@gmail.com>
2020-12-09 02:57:03 -0600

    Create Movie popular route

--- server/routes/movies.route.ts
+++ server/routes/movies.route.ts
@@ -1,4 +1,4 @@
-import { getMovieCredits, getMovieDetails, getMovieImages, getMovieLatest, getMovieNowPlaying, getMovieRecommendations, getMovieReviews, getMovieSimilar, getMovieVideos } from '../controllers';
+import { getMovieCredits, getMovieDetails, getMovieImages, getMovieLatest, getMovieNowPlaying, getMoviePopular, getMovieRecommendations, getMovieReviews, getMovieSimilar, getMovieVideos } from '../controllers';
 
 import { Router } from 'express';
 
@@ -23,3 +23,5 @@ router.get(`${idParams}/videos`, getMovieVideos);
 router.get('/latest', getMovieLatest);
 
 router.get('/now_playing', getMovieNowPlaying);
+
+router.get('/popular', getMoviePopular);
