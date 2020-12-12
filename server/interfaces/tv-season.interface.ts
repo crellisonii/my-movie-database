0661fc5677eb8f68c219f1d433405a32215664f3
Charles Ellison <crelliso@gmail.com>
2020-12-12 13:10:33 -0600

    Create Tv Season interfaces

---
+++ server/interfaces/tv-season.interface.ts
@@ -0,0 +1,12 @@
+// Input Interface
+export interface TvSeasonInput {
+  append_to_response?: string;
+  language?: string;
+}
+
+// Query Parms Interface
+export interface TvSeasonQueryString {
+  api_key: string;
+  append_to_response?: string;
+  language?: string;
+}
