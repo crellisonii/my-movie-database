81d4c573a16a009723cbdf378b135e20c6e2896f
Charles Ellison <crelliso@gmail.com>
2020-12-12 13:22:52 -0600

    Create Tv Season aggregate credits route

--- server/routes/tv-season.route.ts
+++ server/routes/tv-season.route.ts
@@ -1,4 +1,4 @@
-import { getTvSeasonCredits, getTvSeasonDetails } from "../controllers";
+import { getTvSeasonAggregateCredits, getTvSeasonCredits, getTvSeasonDetails } from "../controllers";
 
 import { Router } from "express";
 
@@ -7,5 +7,7 @@ export const router = Router();
 const season = '/:tvId/season/:seasonId';
 
 router.get(season, getTvSeasonDetails);
+
+router.get(`${season}/aggregate_credits`, getTvSeasonAggregateCredits);
 
 router.get(`${season}/credits`, getTvSeasonCredits);
