dcc96060c7e428be4e46812b6db358d0a876afab
Charles Ellison <crelliso@gmail.com>
2020-12-12 11:02:49 -0600

    Create Search movie route

--- server/routes/search.route.ts
+++ server/routes/search.route.ts
@@ -1,6 +1,8 @@
+import { getSearchCollections, getSearchMovies } from '../controllers';
+
 import { Router } from 'express';
-import { getSearchCollections } from '../controllers';
 
 export const router = Router();
 
 router.get('/collection', getSearchCollections);
+router.get('/movie', getSearchMovies);
