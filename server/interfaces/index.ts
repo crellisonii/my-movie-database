6ca40af527ee69eb4f6dc83e7e9aca3d9a81ae7b
Charles Ellison <crelliso@gmail.com>
2020-12-12 09:30:01 -0600

    Create Reviews interfaces

--- server/interfaces/index.ts
+++ server/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './credits.interface';
 export * from './genres.interface';
 export * from './movies.interface';
 export * from './people.interface';
+export * from './reviews.interface';
---
+++ server/interfaces/reviews.interface.ts
@@ -0,0 +1,4 @@
+// Query params interface
+export interface ReviewsQueryString {
+  api_key: string;
+}
