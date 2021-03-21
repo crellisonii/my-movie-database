08d2482ecefad7801e60ae05a230716e3bd08cfb
Charles Ellison <crelliso@gmail.com>
2021-03-21 02:45:04 -0500

    create config and image routes

---
+++ server/routes/configuration.route.ts
@@ -0,0 +1,21 @@
+import {
+  getConfiguration,
+  getConfigurationCountries,
+  getConfigurationJobs,
+  getConfigurationLanguages,
+  getConfigurationTimezones
+} from '../controllers';
+
+import { Router } from 'express';
+
+export const router = Router();
+
+router.get('', getConfiguration);
+
+router.get('/countries', getConfigurationCountries);
+
+router.get('/jobs', getConfigurationJobs);
+
+router.get('/languages', getConfigurationLanguages);
+
+router.get('/timezones', getConfigurationTimezones);
