495737309ccac2a78b57aaceed5a3efc13aaf98b
Charles Ellison <crelliso@gmail.com>
2021-05-28 00:37:27 -0500

    create search-mulitiple models

---
+++ client/src/app/models/apis/search/search-multiple.model.ts
@@ -0,0 +1,26 @@
+import { MovieResult, TVResult } from '../shared';
+
+export interface SearchMultipleInput {
+	include_adult?: boolean;
+	language?: string;
+	page?: number;
+	query: string;
+	region?: string;
+}
+
+interface SearchMultiplePeopleResult {
+	adult?: boolean;
+	id?: number;
+	known_for?: (MovieResult | TVResult)[];
+	media_type: string;
+	name?: string;
+	popularity?: number;
+	profile_path?: string | null;
+}
+
+export interface SearchMultipleResponse {
+	page?: number;
+	results?: (MovieResult | TVResult | SearchMultiplePeopleResult)[];
+	total_pages?: number;
+	total_results?: number;
+}
