19571a378a2c48a47fdf7d2ee3fd6fe7334fef87
Charles Ellison <crelliso@gmail.com>
2021-05-28 04:03:56 -0500

    add service to module

--- client/src/app/modules/app-services.module.ts
+++ client/src/app/modules/app-services.module.ts
@@ -1,11 +1,12 @@
+import { CollectionService, SearchService } from '../services';
+
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { SearchService } from '../services';
 
 @NgModule({
 	declarations: [],
 	imports: [HttpClientModule],
 	exports: [HttpClientModule],
-	providers: [SearchService],
+	providers: [CollectionService, SearchService],
 })
 export class ServicesModule {}
