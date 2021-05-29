439ffb80287abeac91f4ea475ae2efd4e47bce3c
Charles Ellison <crelliso@gmail.com>
2021-05-28 19:11:38 -0500

    fix path params

--- server/controllers/configurations.controller.ts
+++ server/controllers/configurations.controller.ts
@@ -9,17 +9,17 @@ export const getConfiguration = (req: Request, res: Response) => {
 };
 
 export const getConfigurationCountries = (req: Request, res: Response) => {
-	const options = getConfigurationsOptions("GET", "countries");
+	const options = getConfigurationsOptions("GET", "/countries");
 	getTmdbData(res, options);
 };
 
 export const getConfigurationJobs = (req: Request, res: Response) => {
-	const options = getConfigurationsOptions("GET", "jobs");
+	const options = getConfigurationsOptions("GET", "/jobs");
 	getTmdbData(res, options);
 };
 
 export const getConfigurationLanguages = (req: Request, res: Response) => {
-	const options = getConfigurationsOptions("GET", "languages");
+	const options = getConfigurationsOptions("GET", "/languages");
 	getTmdbData(res, options);
 };
 
@@ -27,11 +27,11 @@ export const getConfigurationPrimaryTranslations = (
 	req: Request,
 	res: Response
 ) => {
-	const options = getConfigurationsOptions("GET", "primary_translations");
+	const options = getConfigurationsOptions("GET", "/primary_translations");
 	getTmdbData(res, options);
 };
 
 export const getConfigurationTimezones = (req: Request, res: Response) => {
-	const options = getConfigurationsOptions("GET", "timezones");
+	const options = getConfigurationsOptions("GET", "/timezones");
 	getTmdbData(res, options);
 };
