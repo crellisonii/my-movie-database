0f423dc067f7e5d695ea1dbee38715f48b2363b5
Charles Ellison <crelliso@gmail.com>
2021-05-29 01:17:54 -0500

    create company service and test

---
+++ client/src/app/services/api/company/company.service.ts
@@ -0,0 +1,49 @@
+import {
+	CompanyDetailsInput,
+	CompanyDetailsResponse,
+} from 'src/app/models/apis/companies';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class CompanyService {
+	private baseUrl = `${environment.apiUrl}/company`;
+	private headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+	constructor(private http: HttpClient) {}
+
+	getCompanyAlternativeNames(
+		input: CompanyDetailsInput
+	): Observable<CompanyDetailsResponse> {
+		const { companyId } = input;
+		const url = `${this.baseUrl}/${companyId}/alternative_names`;
+		return this.http.get<CompanyDetailsResponse>(url, {
+			headers: this.headers,
+		});
+	}
+
+	getCompanyDetails(
+		input: CompanyDetailsInput
+	): Observable<CompanyDetailsResponse> {
+		const { companyId } = input;
+		const url = `${this.baseUrl}/${companyId}`;
+		return this.http.get<CompanyDetailsResponse>(url, {
+			headers: this.headers,
+		});
+	}
+
+	getCompanyImages(
+		input: CompanyDetailsInput
+	): Observable<CompanyDetailsResponse> {
+		const { companyId } = input;
+		const url = `${this.baseUrl}/${companyId}/images`;
+		return this.http.get<CompanyDetailsResponse>(url, {
+			headers: this.headers,
+		});
+	}
+}
