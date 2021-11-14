47b61640726f7df469164a6b567db26bfe424288
Charles Ellison <crelliso@gmail.com>
2021-11-13 23:37:27 -0600

    update service

--- server/services/tmdb.service.ts
+++ server/services/tmdb.service.ts
@@ -1,20 +1,21 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { baseURL, imageUrl } from "../constants";
-
 import { Response } from "express";
+import { UrlOptions } from "../interfaces/url-options.interface";
+import { apiKey } from "../env";
+import axios from "axios";
+import { baseURL } from "../constants";
 
-export const getTmdbData = async (
-	res: Response,
-	options: AxiosRequestConfig
-) => {
-	try {
-		const getAxios = axios.create({
-			baseURL,
+export const getTmdbData = async (res: Response, options: UrlOptions) => {
+	const peopleUrl = `${baseURL}${options.pathParams}`;
+	const config = options.axiosConfig;
+	config.params.api_key = apiKey;
+	axios
+		.get(peopleUrl, config)
+		.then((resp) => {
+			res.json(resp.data);
+		})
+		.catch((error) => {
+			if (error.response) {
+				res.json(error);
+			}
 		});
-		const resp = await getAxios(options);
-		res.json(resp.data);
-	} catch (e) {
-		const errorData = e.response.data;
-		res.json(errorData);
-	}
 };
